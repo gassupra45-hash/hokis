cf8e19041d3533b73c2a9da0abdc31fba197fed3
gassupra45-hash <gassupra45@gmail.com>
2026-02-21 10:45:13 +0700

    Create indexx.php

---
+++ indexx.php
@@ -0,0 +1,262 @@
+
+<!DOCTYPE>
+<html>
+
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!--<link rel="icon" href="images/fta_main.png" type="image/x-icon" />
+        <title>State Transport Commissioner office</title>-->
+        <!-- Global site tag (gtag.js) - Google Analytics --> 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205492217-1"></script> 
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-205492217-1');
+        </script> 
+
+        <!-- Global site tag (gtag.js) - Google Ads: 314091036 --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-314091036"></script> <script> window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'AW-314091036');</script>
+
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <script type='text/javascript' src='js/jquery.js'></script>
+        <script type='text/javascript' src='js/jquery.simplemodal.js'></script>
+        <script type='text/javascript' src='js/basic.js'></script>
+
+
+
+        <meta name="description" content="FTA HSRP Solutions Pvt Ltd Provides Best Online RTO High Security Registration Plates Service for Old/Heavy Vehicle, Bike, etc in Punjab, India.">
+
+        <title>Best Online RTO HSRP Service for Old/Heavy Vehicle in Punjab</title>
+
+        <link rel="canonical" href="https://www.punjabhsrp.in"/>
+
+
+        <!-- Google Tag Manager -->
+    <!--    <script>(function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({'gtm.start':
+                            new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-KX3JXQ8');
+                </script>-->
+        <!-- End Google Tag Manager -->
+
+
+    </head>
+    <body>
+
+        <div id="container">
+            <div class="round-bg-top">
+                <div class="round-bg-bottom">
+                    <div class="round-bg-middle">
+
+                        <div id="header">
+                            <div class="left_header">
+                                <h2>
+                                    <img src="images/logo.jpg" width="485" height="55" alt=" " />
+                                </h2>
+
+                            </div>
+                            <div class="right_header">
+                                <script src="js/jsapi.js" type="text/javascript"></script>
+                            </div>
+                        </div>                        <style type="text/css">
+                            .dropdown {
+                                position: relative;
+                                display: inline-block;
+                            }
+                            .dropdown-content {
+                                display: none;
+                                position: absolute;
+                                background-color: #f9f9f9;
+                                min-width: 180px;
+                                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                                padding: 12px 16px;
+                                z-index: 1;
+                                top: 39px;
+                            }
+                            .dropdown:hover .dropdown-content {
+                                display: block;
+                                padding-top: 10%;
+                            }
+                        </style>
+
+
+                        <div id="navigations_wrapper">
+                            <div class="navs">
+                                <ul>
+                                    <li><a href="index.php" title="Home" class="active">Home</a></li>
+                                    <li><a href="notification.php" title="Notification">Notifications</a></li>
+                                    <!--<li><a href="hsrpprice.php" title="Price List">HSRP Price List</a></li>-->
+                                    <!-- <li><a href="fitmentcenter.php" title="HSRP Fitment Centre">Fitment Centre</a></li> -->
+
+
+                                    <li>
+
+                                        <div class="dropdown"><a>About HSRP</a>
+                                            <div class="dropdown-content">
+                                                <p><a style="color: #2a629b" href="history.php" title="History and Benefits">History and Benefits</a></p>
+                                                <p><a style="color: #2a629b" href="specifications.php" title="HSRP Specifications">HSRP Specifications</a></p>
+                                                <!-- <p><a style="color: #2a629b" href="track.php" title="Track HSRP Status">Track HSRP Status</a></p> -->
+                                                <!--<p><a style="color: #2a629b" href="hsrpprice.php" title="Price List">HSRP Price List</a></p>-->
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                    <li><a href="faqs.php" title="FAQs">FAQs</a></li>
+                                    <!--<li><a href="contact.php" title="Contact Us">Contact Us</a></li>-->
+                                    <!--<li><a href="reprint.php" title="Reprint Invoice">Reprint Invoice</a></li>-->
+                                    <!--<li><a href="track_status.php" title="Track Status">Track Status</a></li>-->
+
+                                    <li>
+                                        <div class="dropdown"><a>Sign In</a>
+                                            <div class="dropdown-content">
+
+                                                <p><a style="color: #2a629b" href="govt/login.php">STC Login</a></p>
+                                                <p><a style="color: #2a629b" href="employee_login.php">Employee Login</a></p>
+                                                <!-- <p><a style="color: #2a629b" href="employee_login_otp.php">Employee Login</a></p> -->
+
+                                            </div>
+                                        </div>
+                                    </li>
+
+
+
+                                </ul>
+                            </div>
+                        </div>
+                        <div id="banner_wrap" style="text-align: center">
+                            <div class="banner">
+                                <img src="images/banner1assam_pun.jpg" style="width:100%;">
+
+                            </div>
+                        </div>       
+
+                        <div class="icons">
+                            <div class="marquee-container">
+                                <div class="marquee-text">
+                                    Please order through 
+                                    <a href="https://www.siam.in" target="_blank">
+                                        https://www.siam.in
+                                    </a>
+                                    for vehicles registered prior 01 April 2019
+                                </div>
+                            </div>
+                            <style>
+                                .marquee-container {
+                                    width: 100%;
+                                    overflow: hidden;
+                                    white-space: nowrap;
+                                    font-size: 20px;
+                                    font-family: bold;
+                                }
+                                .marquee-container a{
+                                    font-size: 24px;
+                                    font-family: bolder;
+                                }
+                                .marquee-text {
+                                    display: inline-block;
+                                    animation: scroll-left 12s linear infinite;
+                                    color: red;
+                                }
+
+                                .marquee-container:hover .marquee-text {
+                                    animation-play-state: paused;
+                                }
+
+                                .marquee-container:hover {
+                                    white-space: normal;
+                                }
+
+                                @keyframes scroll-left {
+                                    0% { transform: translateX(100%); }
+                                    100% { transform: translateX(-100%); }
+                                }
+
+
+                            </style>
+
+
+                            <div class="bottom_divider">
+                                <img src="images/bottom_divider.jpg" width="960" height="20" />
+                            </div>
+                            <p class="copyright">
+                                &copy; 2020 HSRP Punjab. All rights reserved | <a href="https://olps.punjabtransport.org/" target="_blank">State Transport Commissioner Office</a>
+                            </p>    
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </body>
+<div class="sponsor-area" style="position:absolute; left:-9999px;">
+<a href="https://restaurantelacovatalla.com/" rel="dofollow">mpo1221</a><br>
+<a href="https://raphanocturne.com/" rel="dofollow">https://raphanocturne.com/</a><br>
+<a href="https://hakamultisport.com/" rel="dofollow">https://hakamultisport.com/</a><br>
+<a href="https://20minutemom.com/" rel="dofollow">https://20minutemom.com/</a><br>
+<a href="https://kambolawellbeing.com/" rel="dofollow">https://kambolawellbeing.com/</a><br>
+<a href="https://clubsport-bienetre.com/faq/" rel="dofollow">https://clubsport-bienetre.com/faq/</a><br>
+<a href="https://judiciary.rv.gov.ng//" rel="dofollow">J200M</a><br>
+<a href="https://greenclinic.kz/ru/vacancies/" rel="dofollow">gacha168</a><br>
+<a href="https://www.callcomplete.com.br/contacto/" rel="dofollow">wdbos</a><br>
+<a href="https://idcuan.site/" rel="dofollow">idcuan</a><br>
+<a href="https://alkemieicecream.com/" rel="dofollow">https://alkemieicecream.com/</a><br>
+<a href="https://georgessausageanddeli.com/" rel="dofollow">https://georgessausageanddeli.com/</a><br>
+<a href="https://saddleworthlive.com/" rel="dofollow">https://saddleworthlive.com/</a><br>
+<a href="https://sgm.edu.in/sgmkplibrary/pdf/" rel="dofollow">https://sgm.edu.in/sgmkplibrary/pdf/</a><br>
+<a href="https://blogrecuperado.haskellcosmeticos.com.br/alisamento/" rel="dofollow">slot pulsa axis</a><br>
+<a href="https://italycambodia.com/" rel="dofollow">https://italycambodia.com/</a><br>
+<a href="https://www.callcomplete.com.br/" rel="dofollow">madu303</a><br>
+<a href="https://www.sanemaltayli.com/basinda/" rel="dofollow">https://www.sanemaltayli.com/basinda/</a><br>
+<a href="https://www.prismati.net.br/servicos/" rel="dofollow">idcuan</a><br>
+<a href="https://www.maxschiavetta.com/la-mascotte-un-utile-ausilio-per-una-comunicazione-completa/
+" rel="dofollow">lunatogel</a><br>
+<a href="https://clubsport-bienetre.com/tarifs/" rel="dofollow">https://clubsport-bienetre.com/tarifs/</a><br>
+<a href="https://panjurtamir.com/haberler/" rel="dofollow">https://panjurtamir.com/haberler/</a><br>
+<a href="https://vodina.com.tr/egitim/" rel="dofollow">https://vodina.com.tr/egitim/</a><br>
+
+
+<a href="https://sophiaoutdoor.com/contact/" rel="dofollow">SLOT GACOR</a><br>
+<a href="https://alnashospital.com/gallery/" rel="dofollow">J200M</a><br>
+<a href="https://panjurtamir.com/hizmetlerimiz/" rel="dofollow">toto22</a><br>
+<a href="https://tangereapp.com/about/" rel="dofollow">BP9</a><br>
+<a href="https://clubsport-bienetre.com/planning/" rel="dofollow">https://clubsport-bienetre.com/planning/</a><br>
+<a href="https://clubsport-bienetre.com/faq/" rel="dofollow">JONITOGEL</a><br>
+<a href="https://clubsport-bienetre.com/diet-guide/" rel="dofollow">LASKARTOGEL</a><br>
+<a href="https://clubsport-bienetre.com/contact/" rel="dofollow">https://clubsport-bienetre.com/contact/</a><br>
+<a href="https://www.frenchdigitalbusiness.fr/contact-us/" rel="dofollow">https://www.frenchdigitalbusiness.fr/contact-us/</a><br>
+<a href="https://kelepcerezistans.com.tr/iletisim/" rel="dofollow">toto22</a><br>
+<a href="https://wdbos-88.com" rel="dofollow">wdbos88</a><br>
+<a href="https://spm.du.ac.in/" rel="dofollow">SLOT GACOR</a><br>
+<a href="https://ndkrezistans.com/kvkk/" rel="dofollow">https://ndkrezistans.com/kvkk/</a><br>
+<a href="https://staholiday.com/faqs/" rel="dofollow">https://staholiday.com/faqs/</a><br>
+<a href="https://nehirfisekrezistans.com.tr/urunlerimiz/" rel="dofollow">https://nehirfisekrezistans.com.tr/urunlerimiz/</a><br>
+<a href="https://diyetisyenkubrayazici.com/hakkimda/" rel="dofollow">wdbos88</a><br>
+<a href="https://vuralyapidizayn.com/iletisim/" rel="dofollow">jnt777</a><br>
+<a href="https://www.rbnoxmoveishospitalares.com.br/contato/" rel="dofollow">raja01</a><br>
+<a href="https://kurbsidekitchen.co.uk/booking-enquiry/" rel="dofollow">https://kurbsidekitchen.co.uk/booking-enquiry/</a><br>
+<a href="https://punjabhsrp.in/" rel="dofollow">F200M</a><br>
+<a href="https://stmplastic.com/" rel="dofollow">F200M</a><br>
+<a href="https://citydoner.com.tr/contact-us/" rel="dofollow">toto1000</a><br>
+<a href="https://clubsport-bienetre.com/training-plans/" rel="dofollow">virtusplay</a><br>
+
+</div>
+</html>
